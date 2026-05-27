522c8a6ac060740cf9fdbe896f651bcc846a2e10
Leyzzi11 <zeyroxxyacine2009@gmail.com>
2026-05-27 23:44:02 +0100

    Update pch.hpp

--- clicker/pch.hpp
+++ clicker/pch.hpp
@@ -1,16 +1,12 @@
 #pragma once
-
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
-
 #include <iostream>
 #include <thread>
 #include <random>
-
+#include <chrono>
 #include <Windows.h>
-
 using namespace std::chrono_literals;
-
 #include "console.hpp"
 #include "utils.hpp"
 #include "config.hpp"
